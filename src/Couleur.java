da344d7cb2d0e2aa90283c1df21188315ef9c3f0
GouNicolas <nicolas.gouillardon@utbm.fr>
2025-01-11 14:23:18 +0100

    javadoc bat, joueur,ressources,couleur+de,de,tuile,plateau

--- src/Couleur.java
+++ src/Couleur.java
@@ -1,8 +1,18 @@
+/**
+ * Énumération représentant les couleurs des branches de l'UTBM.
+ * ROUGE pour GMC, BLANC pour EDIM, JAUNE pour INFO.
+ */
 enum Couleur {
-    ROUGE,
-    BLANC,
-    JAUNE;
+    ROUGE,  // Représente la branche GMC
+    BLANC,  // Représente la branche EDIM
+    JAUNE;  // Représente la branche INFO
 
+    /**
+     * Convertit une CouleurDe en Couleur
+     * @param couleurDe La couleur du dé à convertir
+     * @return La couleur correspondante
+     * @throws IllegalArgumentException si la couleur du dé n'est pas reconnue
+     */
     public static Couleur fromCouleurDe(CouleurDe couleurDe) {
         switch (couleurDe) {
             case ROUGE:
